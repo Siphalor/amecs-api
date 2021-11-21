59e86b6b973dea1e0e32f900006cd9e3f15f897f
Siphalor <info@siphalor.de>
2021-11-21 15:58:41 +0100

    Fix issues when upgrading scroll keys from old options.txt
    Fixes Siphalor/mouse-wheelie#146

--- src/main/java/de/siphalor/amecs/impl/mixin/MixinInputUtilType.java
+++ src/main/java/de/siphalor/amecs/impl/mixin/MixinInputUtilType.java
@@ -1,15 +1,15 @@
 package de.siphalor.amecs.impl.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import de.siphalor.amecs.api.KeyBindingUtils;
 import de.siphalor.amecs.impl.AmecsAPI;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.util.InputUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(InputUtil.Type.class)
@@ -17,7 +17,16 @@ public abstract class MixinInputUtilType {
 	@SuppressWarnings("UnresolvedMixinReference")
 	@Inject(method = "<clinit>", at = @At("RETURN"))
 	private static void onRegisterKeyCodes(CallbackInfo callbackInfo) {
-		InputUtil.Type.mapKey(InputUtil.Type.MOUSE, AmecsAPI.makeKeyID("mouse.scroll.up"), KeyBindingUtils.MOUSE_SCROLL_UP);
-		InputUtil.Type.mapKey(InputUtil.Type.MOUSE, AmecsAPI.makeKeyID("mouse.scroll.down"), KeyBindingUtils.MOUSE_SCROLL_DOWN);
+		createScrollKey("mouse.scroll.up", KeyBindingUtils.MOUSE_SCROLL_UP);
+		createScrollKey("mouse.scroll.down", KeyBindingUtils.MOUSE_SCROLL_DOWN);
+	}
+
+	@Unique
+	private static void createScrollKey(String name, int keyCode) {
+		String keyName = AmecsAPI.makeKeyID(name);
+		InputUtil.Type.mapKey(InputUtil.Type.MOUSE, keyName, keyCode);
+
+		// Legacy compatibility (amecsapi <1.3)
+		InputUtil.KeyCode.NAMES.put("amecsapi.key." + name, InputUtil.fromName(keyName));
 	}
 }
